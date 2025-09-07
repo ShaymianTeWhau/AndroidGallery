849e9f2786221cc22b60da89fedf055a63f20e48
ShaymianTeWhau <shaytewhau@gmail.com>
2025-09-08 09:25:19 +1200

    center the no permission screen

--- app/src/main/java/com/example/gallery_a2_159336_21005190/MainActivity.kt
+++ app/src/main/java/com/example/gallery_a2_159336_21005190/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -42,6 +43,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
@@ -114,7 +116,8 @@ class MainActivity : ComponentActivity() {
                 modifier = modifier
                     .fillMaxSize()
                     .padding(24.dp),
-                verticalArrangement = Arrangement.spacedBy(12.dp)
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Text(
                     "To show your photos, allow access to images.",
