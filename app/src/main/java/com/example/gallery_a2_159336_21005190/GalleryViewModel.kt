63c45c0dd744223f6ec36911270fc39b463eaed1
ShaymianTeWhau <shaytewhau@gmail.com>
2025-09-16 14:39:24 +1200

    move getImagesData() to the viewModel

--- app/src/main/java/com/example/gallery_a2_159336_21005190/GalleryViewModel.kt
+++ app/src/main/java/com/example/gallery_a2_159336_21005190/GalleryViewModel.kt
@@ -1,6 +1,57 @@
 package com.example.gallery_a2_159336_21005190
 
+import android.content.ContentResolver
+import android.provider.MediaStore
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class GalleryViewModel : ViewModel(){
+
+    var photos = mutableListOf<List<PhotoData>>(emptyList())
+        private set
+
+    fun loadImages(contentResolver: ContentResolver){
+        viewModelScope.launch(Dispatchers.IO){
+
+        }
+    }
+
+    fun getImagesData(contentResolver: ContentResolver): List<PhotoData> {
+        val photoData = mutableListOf<PhotoData>()
+
+        val projection = arrayOf(
+            MediaStore.Images.Media._ID,
+            MediaStore.Images.Media.ORIENTATION,
+            MediaStore.Images.Media.WIDTH,
+            MediaStore.Images.Media.HEIGHT
+        )
+        val sortOrder = "${MediaStore.Images.Media.DATE_ADDED} DESC"
+
+        contentResolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            projection,
+            null,
+            null,
+            sortOrder
+        )?.use { cursor ->
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
+            val orientationColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.ORIENTATION)
+            val widthColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.WIDTH)
+            val heightColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.HEIGHT)
+            while (cursor.moveToNext()) {
+                photoData.add(
+                    PhotoData(
+                        id = cursor.getLong(idColumn),
+                        orientation = cursor.getInt(orientationColumn),
+                        width = cursor.getInt(widthColumn),
+                        height = cursor.getInt(heightColumn)
+                    )
+                )
+            }
+        }
+
+        return photoData
+    }
 }
--- app/src/main/java/com/example/gallery_a2_159336_21005190/MainActivity.kt
+++ app/src/main/java/com/example/gallery_a2_159336_21005190/MainActivity.kt
@@ -168,7 +168,7 @@ class MainActivity : ComponentActivity() {
         var photosData by remember { mutableStateOf<List<PhotoData>>(emptyList()) }
         LaunchedEffect(Unit){
             withContext(Dispatchers.IO){
-                photosData = getImagesData(contentResolver)
+                photosData = viewModel.getImagesData(contentResolver)
                 println("Found ${photosData.size} images")
             }
         }
@@ -375,41 +375,5 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    fun getImagesData(contentResolver: ContentResolver): List<PhotoData> {
-        val photoData = mutableListOf<PhotoData>()
-
-        val projection = arrayOf(
-            MediaStore.Images.Media._ID,
-            MediaStore.Images.Media.ORIENTATION,
-            MediaStore.Images.Media.WIDTH,
-            MediaStore.Images.Media.HEIGHT
-            )
-        val sortOrder = "${MediaStore.Images.Media.DATE_ADDED} DESC"
-
-        contentResolver.query(
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-            projection,
-            null,
-            null,
-            sortOrder
-        )?.use { cursor ->
-            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID)
-            val orientationColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.ORIENTATION)
-            val widthColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.WIDTH)
-            val heightColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.HEIGHT)
-            while (cursor.moveToNext()) {
-                photoData.add(
-                    PhotoData(
-                        id = cursor.getLong(idColumn),
-                        orientation = cursor.getInt(orientationColumn),
-                        width = cursor.getInt(widthColumn),
-                        height = cursor.getInt(heightColumn)
-                    )
-                )
-            }
-        }
-
-        return photoData
-    }
 }
 
